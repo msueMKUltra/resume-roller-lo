ac12688b4758f5f5eb381f2f5eea298cf7808c48
Roller Lo <jasonbh02@gmail.com>
2019-08-16 02:22:18 +0800

    Updates

---
+++ precache-manifest.9cfd48b31d545e70d10e620049deea6f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7753d72dcaceb7a7f56cbd32e8f65a66",
+    "url": "/resume-roller-lo/index.html"
+  },
+  {
+    "revision": "1fe362085624911eb0d8",
+    "url": "/resume-roller-lo/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "175cfc85d4037b83088e",
+    "url": "/resume-roller-lo/static/css/main.7e631550.chunk.css"
+  },
+  {
+    "revision": "1fe362085624911eb0d8",
+    "url": "/resume-roller-lo/static/js/2.c5e44d61.chunk.js"
+  },
+  {
+    "revision": "175cfc85d4037b83088e",
+    "url": "/resume-roller-lo/static/js/main.92da21e4.chunk.js"
+  },
+  {
+    "revision": "f3b6550598fd03c93109",
+    "url": "/resume-roller-lo/static/js/runtime~main.d4cd0d81.js"
+  },
+  {
+    "revision": "cd8979e58f7b5b30c1e17c2ccd6ff1d2",
+    "url": "/resume-roller-lo/static/media/experience.cd8979e5.png"
+  },
+  {
+    "revision": "c54971e1c52b180c5e33e96af99d5fdb",
+    "url": "/resume-roller-lo/static/media/photoshop.c54971e1.png"
+  },
+  {
+    "revision": "0dedd35d06cf0ae46b662f8452e2825c",
+    "url": "/resume-roller-lo/static/media/resume-background.0dedd35d.jpg"
+  }
+]);
